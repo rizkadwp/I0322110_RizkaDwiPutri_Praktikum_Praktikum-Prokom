6563aeb39b1b94f3ec8dd644282d30e838955390
rizkadwp <126968188+rizkadwp@users.noreply.github.com>
2023-03-26 18:49:23 +0700

    Add files via upload

---
+++ Tugas Prokom 1.py
@@ -0,0 +1,8 @@
+nama = "Rizka Dwi Putri"
+nim = "I0322110"
+tempat_tinggal = "Saya tinggal di kota Yogyakarta, kota yang memang istimewa bagi saya dengan segala cerita yang ada. Kota yang bersih dan juga nyaman. Selalu menjadi tempat terbaik untuk pulang"
+kampung_halaman = "Kampung halaman saya adalah Cikampek yang terletak di provinsi Jawa Barat. Tempat masa kecilku dan terlalu banyak kebahagiaan, rasa aman dan juga kenyamanan yang terukir disana"
+print("Nama: ", nama)
+print("NIM: ", nim)
+print("Tempat Tinggal: ", tempat_tinggal)
+print("Kampung Halaman: ", kampung_halaman)
